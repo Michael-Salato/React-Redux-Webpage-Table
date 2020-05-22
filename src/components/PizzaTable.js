8be75cc35ba92869423db4091b575bc66e410e36
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 15:43:05 -0500

    moved css for PizzaTable to App.css

--- src/components/PizzaTable.js
+++ src/components/PizzaTable.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 import NewPizza from "./NewPizza";
-import "./PizzaTable.css";
 
 function PizzaTable() {
   return (
