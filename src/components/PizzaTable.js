669240dac625756e4ff6117ff037dc5f28379f89
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 15:39:13 -0500

    added CSS to PizzaTable

--- src/components/PizzaTable.js
+++ src/components/PizzaTable.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 import NewPizza from "./NewPizza";
+import "./PizzaTable.css";
 
 function PizzaTable() {
   return (
     <Container fluid>
-      <Row justify="center" debug>
-        <Col sm={1} debug>
+      <Row justify="center">
+        <Col className="pizza-table-col" sm={1}>
           Pepperoni
         </Col>
-        <Col sm={1} debug>
+        <Col className="pizza-table-col" sm={1}>
           Sausage
         </Col>
-        <Col sm={1} debug>
+        <Col className="pizza-table-col" sm={1}>
           Spinach
         </Col>
-        <Col sm={1} debug>
+        <Col className="pizza-table-col" sm={1}>
           Size
         </Col>
-        <Col sm={2} debug>
+        <Col className="pizza-table-col" sm={2}>
           Special Instructions
         </Col>
-        <Col sm={1} debug>
+        <Col className="pizza-table-col" sm={1}>
           Amount
         </Col>
       </Row>
