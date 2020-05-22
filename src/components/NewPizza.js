c9614b58e432a160c572efcb6951b207509eca39
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 15:08:28 -0500

    added alerts to buttons

--- src/components/NewPizza.js
+++ src/components/NewPizza.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import {Row, Col} from 'react-grid-system';
+import React from "react";
+import { Row, Col } from "react-grid-system";
 
 function NewPizza() {
-    return(
-        <Row justify="center" debug>
-            <Col sm={1} debug>
-                <input type="checkbox" />
-            </Col>
-            <Col sm={1} debug>
-                <input type="checkbox" />
-            </Col>
-            <Col sm={1} debug>
-                <input type="checkbox" />
-            </Col>
-            <Col sm={1} debug>
-                <select>
-                    <option value="small">Small</option>
-                    <option value="medium">Medium</option>
-                    <option value="large">Large</option>
-                </select>
-            </Col>
-            <Col sm={2} debug>
-                <input type="text" />
-            </Col>
-            <Col sm={1} debug>
-                <input type="number" min="0" max="10"/>
-            </Col>
-        </Row>
-    );
+  return (
+    <Row justify="center" debug>
+      <Col sm={1} debug>
+        <input type="checkbox" />
+      </Col>
+      <Col sm={1} debug>
+        <input type="checkbox" />
+      </Col>
+      <Col sm={1} debug>
+        <input type="checkbox" />
+      </Col>
+      <Col sm={1} debug>
+        <select>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select>
+      </Col>
+      <Col sm={2} debug>
+        <input type="text" />
+      </Col>
+      <Col sm={1} debug>
+        <input type="number" min="0" max="10" />
+      </Col>
+    </Row>
+  );
 }
 
 export default NewPizza;
--- src/components/UserActionsBar.js
+++ src/components/UserActionsBar.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 
-function UserActionsBars() {
-  return (
-    <Container fluid>
-      <Row justify="center" debug>
-        <button>Submit Order</button>&nbsp;&nbsp;
-        <button>Order History</button>
-      </Row>
-    </Container>
-  );
+class UserActionsBars extends React.Component {
+  submitOrder() {
+    alert("Order Submitted!");
+  }
+
+  orderHistory() {
+    alert("Opening Order History...");
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row justify="center">
+          <button onClick={this.submitOrder}>Submit Order</button>&nbsp;&nbsp;
+          <button onClick={this.orderHistory}>Order History</button>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default UserActionsBars;
