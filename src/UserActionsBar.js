8b94614257d7a2fd468d1ea5de0ee2de5930973d
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 14:01:12 -0500

    removed New Order button

--- src/UserActionsBar.js
+++ src/UserActionsBar.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import {Container, Row, Col} from 'react-grid-system';
+import React from "react";
+import { Container, Row, Col } from "react-grid-system";
 
 function UserActionsBars() {
-    return(
-        <Container fluid>
-            <Row justify="center" debug>
-                <button>Submit Order</button>&nbsp;&nbsp;
-                <button>New Order</button>&nbsp;&nbsp;
-                <button>Order History</button>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container fluid>
+      <Row justify="center" debug>
+        <button>Submit Order</button>&nbsp;&nbsp;
+        <button>Order History</button>
+      </Row>
+    </Container>
+  );
 }
 
 export default UserActionsBars;
