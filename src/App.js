bf76d3e0a51c6e04606f11f9f662a01772b2f01f
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 14:16:28 -0500

    added Header & Footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import PizzaTable from "./components/PizzaTable";
 import UserActionsBar from "./UserActionsBar";
 import { ScreenClassProvider, Container, Row, Col } from "react-grid-system";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ScreenClassProvider className="App">
+      <Header />
       <PizzaTable />
       <br />
       <UserActionsBar />
+      <Footer />
     </ScreenClassProvider>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return <header className="header">Welcome to the Pizzeria!</header>;
+  }
+}
+
+export default Header;
