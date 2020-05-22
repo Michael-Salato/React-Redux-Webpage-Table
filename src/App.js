c647bf7d72c217ee53f6469c351e38fd0e891d98
Michael Salato <michael.salato@accenturefederal.com>
2020-05-22 14:09:44 -0500

    moved NewPizza & PizzaTable into components folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import NewPizza from './NewPizza';
-import PizzaTable from './PizzaTable';
-import UserActionsBar from './UserActionsBar';
-import {ScreenClassProvider, Container, Row, Col} from 'react-grid-system';
+import React from "react";
+import "./App.css";
+import PizzaTable from "./components/PizzaTable";
+import UserActionsBar from "./UserActionsBar";
+import { ScreenClassProvider, Container, Row, Col } from "react-grid-system";
 
 function App() {
   return (
     <ScreenClassProvider className="App">
-      <PizzaTable /><br />
+      <PizzaTable />
+      <br />
       <UserActionsBar />
     </ScreenClassProvider>
   );
