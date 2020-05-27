a913741e63389301714f9123d6886f45c8eae5eb
Michael Salato <michael.salato@accenturefederal.com>
2020-05-27 14:06:30 -0500

    removed redux form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import pizzaReducer from "./pizzaReducer";
-import { reducer as formReducer } from "redux-form";
 
 const allReducers = combineReducers({
   pizzas: pizzaReducer,
-  form: formReducer,
 });
 
 export default allReducers;
