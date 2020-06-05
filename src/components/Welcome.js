3c6ca6d2926367c339d8e1236c8efd4eddea1d76
Michael Salato <michael.salato@accenturefederal.com>
2020-06-05 14:50:35 -0500

    added redirect from homepage button

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Welcome() {
-  return <h1 style={{ textAlign: "center" }}>Welcome to the Pizzeria!</h1>;
+function Welcome(props) {
+  return (
+    <React.Fragment>
+      <h1 style={{ textAlign: "center" }}>Welcome to the Pizzeria!</h1>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => props.history.push("/orderpizza")}>
+          Order Pizza
+        </button>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Welcome;
