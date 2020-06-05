54231080d0701e29ccb0779a3cc7be2936fa231a
Michael Salato <michael.salato@accenturefederal.com>
2020-06-05 09:49:46 -0500

    added order history dummy data

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Container, Row, Col } from "react-grid-system";
+import orderHistory from "../orderHistory";
 
 function OrderHistory() {
-  return <h1>Order History</h1>;
+  return (
+    <Container fluid>
+      <h1>Order History</h1>
+      <Row justify="center">
+        <Col className="pizza-table-col" sm={2}>
+          Toppings
+        </Col>
+        <Col className="pizza-table-col" sm={1}>
+          Size
+        </Col>
+        <Col className="pizza-table-col" sm={2}>
+          Special Instructions
+        </Col>
+        <Col className="pizza-table-col" sm={1}>
+          Amount
+        </Col>
+      </Row>
+      {orderHistory.map((order, index) => (
+        <Row key={"orderHistory-" + index} justify="center">
+          <Col sm={2}>
+            <p>
+              {order.pepperoni ? "Pepperoni" : null} {order.sausage}
+            </p>
+          </Col>
+          <Col sm={1}>
+            <p>{order.size}</p>
+          </Col>
+          <Col sm={2}>
+            <p>{order.instructions}</p>
+          </Col>
+          <Col sm={1}>
+            <p>{order.amount}</p>
+          </Col>
+        </Row>
+      ))}
+      <p>{JSON.stringify(orderHistory)}</p>
+    </Container>
+  );
 }
 
 export default OrderHistory;
---
+++ src/orderHistory.js
@@ -0,0 +1,28 @@
+const orderHistory = [
+  {
+    pepperoni: false,
+    sausage: true,
+    spinach: true,
+    size: "medium",
+    instructions: "",
+    amount: "1",
+  },
+  {
+    pepperoni: true,
+    sausage: true,
+    spinach: false,
+    size: "small",
+    instructions: "Half sausage, half pepperoni",
+    amount: "1",
+  },
+  {
+    pepperoni: false,
+    sausage: false,
+    spinach: false,
+    size: "large",
+    instructions: "",
+    amount: "2",
+  },
+];
+
+export default orderHistory;
